cdd8ce2a1becf6f3a82190488c01eaf29f564412
Jesus_Ramos <jesuslramos03@gmail.com>
2025-11-26 15:52:34 -0400

    Este es mi primer commit

---
+++ hello_git.py
@@ -0,0 +1,10 @@
+print("Hello Git")
+
+"""
+
+Se empieza a utilizar GIT con el comando:
+"git init" dentro de la carpeta principal del proyecto
+
+https://www.youtube.com/watch?v=3GymExBkKjE
+min 42.10
+"""
