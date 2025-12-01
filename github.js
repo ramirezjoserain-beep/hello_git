55d0b8e7ed940c1f1cf3335f770ecd5db1fed37f
Jesus_Ramos <jesuslramos03@gmail.com>
2025-12-01 05:11:55 -0400

    Creacion de Clave SSH

--- github.js
+++ github.js
@@ -4,4 +4,12 @@
     guardadas en /home/usuario/.ssh/, el comando
     para crear la clave es:
         ssh-keygen -t ed25519 -C "youremail@example.com"
+        ssh -T git@github.com
+# Attempts to ssh to GitHub
+
+
+
+
+https://www.youtube.com/watch?v=3GymExBkKjE
+min 3:00:00
 */ 
