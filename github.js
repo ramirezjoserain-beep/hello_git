fa0b83ebbf7f04ed8ea2f84286f5fa1110727422
Jesus_Ramos <jesuslramos03@gmail.com>
2025-12-01 04:49:02 -0400

    Introduccion Github && SSH

---
+++ github.js
@@ -0,0 +1,7 @@
+/*
+**CURSO DE GIIHUB CONTINUACION DE GIT**
+    Se va a utilizar las claves SSH, van a estar
+    guardadas en /home/usuario/.ssh/, el comando
+    para crear la clave es:
+        ssh-keygen -t ed25519 -C "youremail@example.com"
+*/ 
